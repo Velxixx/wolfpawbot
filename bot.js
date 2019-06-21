f98372d987a15e124925c9428c4ef50961e6b6b9
Lone <rickynorat18@gmail.com>
2019-06-21 18:52:25 -0400

    Test Commit

--- bot.js
+++ bot.js
@@ -1,8 +1,21 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require("./config.json")
+const config = require('./config.json')
 
 let token = config.token
+let version = ('On Progress')
 
-client.login(token)
+ client.login(token)
+
+ client.on("ready", () => {
+    console.log("Bot Wolf Paws Online and Ready to Go!");
+ 
+ });
+
+ client.on("message", (message) => {
+    if(message.content.startsWith(prefix+"test")) {
+        message.channel.send("functioning")
+
+ }});
+
 

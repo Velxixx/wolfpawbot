dcd5b1a78ff263dbe536cfd2b12f811444d964ed
Lone <rickynorat18@gmail.com>
2019-06-21 19:36:58 -0400

    Error Fix arg Variable

--- bot.js
+++ bot.js
@@ -2,12 +2,12 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json')
 
-let token = config.token
-let version = ('On Progress')
-let prefix = config.prefix
-let args = message.content.substring(prefix.lenght).split(" "); 
+let token = config.token;
+let version = ('On Progress');
+let prefix = config.prefix;
+let args = message.content.substring(prefix.length).split(" ")
 
- client.login(token);
+ client.login(token)
 
  client.on("ready", () => {
     console.log("Bot Wolf Paws Online and Ready to Go!");

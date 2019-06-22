ba27060624541393c28fc49615c3357200f80d88
Lone <rickynorat18@gmail.com>
2019-06-22 13:26:02 -0400

    Args and begining of embeds

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json')
 
+let token = config.token
+
  client.login(token)
 
  client.on("ready", () => {

86af65fe1cc6533f7174cf6d4b99b944092cba9e
Lone <rickynorat18@gmail.com>
2019-06-23 18:03:59 -0400

    Embeds help, Version, Ping

--- bot.js
+++ bot.js
@@ -1,47 +1,50 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
-const config = require('./config.json')
+const bot = new Discord.Client();
+const cfg = require('./config.json')
 
-let token = config.token
+  bot.on("ready", () => {
+    console.log(`Logged in as ${bot.user.tag} (${bot.user.id}) on ${bot.guilds.size} servers`);
+   });
 
- client.login(token)
-
- client.on("ready", () => {
-    console.log("Bot Wolf Paws Online and Ready to Go!");
+  bot.on('message', msg => {
+    if (msg.author.bot || !msg.content.startsWith(cfg.prefix)) return;
+    const args = msg.content.slice(cfg.prefix.length).split(' ')
+    const command = args.shift().toLowerCase();
+    if(command === 'ping') {
+      const then = Date.now();
+      msg.channel.send('Pinging...').then(m => {
+        m.edit(`Pong! It took ${Date.now() - then}ms to send that message\nHeartbeat ${bot.ping}ms`)
+      })
+    }
+    if(command === 'help') {
+      const embed = new Discord.RichEmbed(1) 
+    .setTitle("Help Section")
+    .setAuthor(msg.author.username, msg.author.displayAvatarURL)
+    .setColor(0xffff00)
+    .setDescription("Here are all the commands **__`available`__** for the bot remember to use the **__`prefix`__** **__`!w`__** before executing the **__`command`__**.")
+    .setFooter("", bot.user.avatarURL)
+    .setImage()
+    .setThumbnail(msg.author.displayAvatarURL)
+    .setTimestamp()
+    .setURL("")
+    .addField("Social", "**`instagram`**  **`twitter`**  **`discord`**", true)
+    .addField("Games", "**`non yet`**", true)
+    .addField("Moderation", "**`ban`**  **`mute`**  **`tempmute`**",true)
+    .addField("Random Commands", "**`wolf`**", true)
+    .addField("General Info", "**`ping`** **`version`**")
+    msg.channel.send({embed})
+    console.log(`----Command Executed----\nArgs ${args}\nCommand: ${command}`)
+    }
+    if(command === 'version') {
+      var version = ('`On progress`')
+      msg.channel.send(version)
+    }
   });
+  
+    bot.login(cfg.token)
 
- let prefix = config.prefix
 
-   client.on("message", (message) => {
-        if(message.content.startsWith(prefix+"test")) {
-            message.channel.send("Online!")
-    
-let ping = client.ping
-let version = ('On Progress')
 
-    switch(args[0]){
-      case  prefix+'ping':
-        message.reply(ping)
-        break;
-      case prefix+'social':
-       if(args[1] === 'instagram'){
-        message.channel.send('https://www.instagram.com/lonewolfdankmemes/')
-        break;
-      }
-      case prefix+'info' : 
-       if(args[1] === 'version'){(
-        message.channel.send("Version "+version))
-        break;
-        }
-      case prefix+"help":
-        var embed = new Discord.RichEmbed()
-          .addField("Wolf Command", "Awesome Wolf Pictures")
-        message.channel.sendEmbed(embed);
-        break;
-      } 
-    }     
-});
-    
 
   
         

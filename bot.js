f9a935950e17edee7fa979000a48bda461096691
Lone <rickynorat18@gmail.com>
2019-06-26 11:48:37 -0400

    Commands, clear, help update, spam, status changes

--- bot.js
+++ bot.js
@@ -1,12 +1,22 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const cfg = require('./config.json')
-
-  bot.on("ready", () => {
+//         BOT ON / CONSOLE
+  bot.on("ready", async () => {
     console.log(`Logged in as ${bot.user.tag} (${bot.user.id}) on ${bot.guilds.size} servers`);
+// STATUS
+    let statuses =[ 'w!help || On Progress...',
+                   `on ${bot.guilds.size} servers`,
+                   `with ${bot.users.size} wolves`
+                  ]
+    setInterval(function() {
+      let status = statuses[Math.floor(Math.random() * statuses.length)];
+      bot.user.setActivity(status, {type: 'STREAMING'}, `${bot.user.setStatus('dnd')}`)
+    }, 15000)
+  
    });
-
-  bot.on('message', msg => {
+//        BOT MESSAGE / MSG
+  bot.on('message', async msg => {
     if (msg.author.bot || !msg.content.startsWith(cfg.prefix)) return;
     const args = msg.content.slice(cfg.prefix.length).split(' ')
     const command = args.shift().toLowerCase();
@@ -16,32 +26,55 @@ const cfg = require('./config.json')
         m.edit(`Pong! It took ${Date.now() - then}ms to send that message\nHeartbeat ${bot.ping}ms`)
       })
     }
+//           HELP
     if(command === 'help') {
-      const embed = new Discord.RichEmbed(1) 
+      const embed = new Discord.RichEmbed() 
     .setTitle("Help Section")
     .setAuthor(msg.author.username, msg.author.displayAvatarURL)
-    .setColor(0xffff00)
-    .setDescription("Here are all the commands **__`available`__** for the bot remember to use the **__`prefix`__** **__`!w`__** before executing the **__`command`__**.")
-    .setFooter("", bot.user.avatarURL)
+    .setColor(0xff0000)
+    .setDescription("Here are all the commands **__`available`__** for the bot remember to use the **__`prefix`__** **__`!w`__** before executing the **__`command`__**. The ones that are crossed out are not fully implemented yet. ")
+    .setFooter(`Requested by ${msg.author.username}`, bot.user.avatarURL)
     .setImage()
     .setThumbnail(msg.author.displayAvatarURL)
     .setTimestamp()
     .setURL("")
-    .addField("Social", "**`instagram`**  **`twitter`**  **`discord`**", true)
-    .addField("Games", "**`non yet`**", true)
-    .addField("Moderation", "**`ban`**  **`mute`**  **`tempmute`**",true)
-    .addField("Random Commands", "**`wolf`**", true)
+    .addField("Social", "||**`instagram`**||  ||**`twitter`**||  ||**`discord`**||", true)
+    .addField("Games", "||**`non yet`**||", true)
+    .addField("Moderation", "**`clear`** ||**`ban`**||  ||**`mute`**||  ||**`tempmute`**||",true)
+    .addField("Random Commands", "**`wolf`** **`spam`**", true)
     .addField("General Info", "**`ping`** **`version`**")
     msg.channel.send({embed})
-    console.log(`----Command Executed----\nArgs ${args}\nCommand: ${command}`)
-    }
+     }
     if(command === 'version') {
       var version = ('`On progress`')
       msg.channel.send(version)
     }
+//            SPAM
+    let words = (['Kermit Has A French Frie'])
+
+if (command === 'spam') {
+setInterval(function() {
+let word_spam = words[Math.floor(Math.random() * words.length)];
+msg.channel.send(word_spam)
+});
+
+}
+//           CLEAR
+if(command === "clear") {
+  if(!msg.member.hasPermission('MANAGE_MESSAGES')) return msg.reply("`You don't have the requirements to use this command.`")
+  if(!args[0]) return msg.channel.send("`Error! Please specify the amount of messages to delete`");
+  msg.channel.bulkDelete(args[0]).then(() => {
+    msg.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
+  });
+}
+
+
+       
   });
   
+  
     bot.login(cfg.token)
+    
 
 
 

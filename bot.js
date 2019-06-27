8778ffed6b9d1cbf5486c1dcd037bf94052600b7
Lone <rickynorat18@gmail.com>
2019-06-27 00:12:56 -0400

    Emoji Fix

--- bot.js
+++ bot.js
@@ -77,7 +77,7 @@ const user = msg.mentions.users.first();
          user = msg.guild.members.get(args.slice(0, 1).join(' '));
          user = user.user;
     }catch (error){
-        return msg.reply('`❌ Invalid User ID`');
+        return msg.reply('` 🙋‍ Please specify the person you want to ban!`')
     }
   }
   
